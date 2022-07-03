032303b9b25597719e8139d0d86a7a01556143d0
AZProductions <66299945+AZProductions@users.noreply.github.com>
2022-07-03 15:36:21 +0200

    chore: updated index page

--- vacaro-launcher/src/routes/index.svelte
+++ vacaro-launcher/src/routes/index.svelte
@@ -1,16 +1,45 @@
 <script>
-	import { Grid, Row, Column, Tile, AspectRatio } from 'carbon-components-svelte';
+	import {
+		Grid,
+		Row,
+		Column,
+		Tile,
+		AspectRatio,
+		ButtonSet,
+		Dropdown,
+		Button
+	} from 'carbon-components-svelte';
+	import Launch from 'carbon-icons-svelte/lib/Launch.svelte';
 	import FAQ from '../components/home/FAQ.svelte';
 	import YT from '../components/home/YT.svelte';
 </script>
 
-<Tile>
+<Tile style="height: 400px;">
 	<Grid>
 		<Row>
 			<Column>Column</Column>
 			<Column>Column</Column>
 			<Column>Column</Column>
-			<Column>Column</Column>
+			<Column sm={{ offset: 10 }} >
+				<ButtonSet stacked>
+					<Button icon={Launch}>Launch Engine</Button>
+					<Dropdown
+						hideLabel
+						light
+						warn
+						warnText="Warning, the Vacaro Launcher is still in early development."
+						type="default"
+						style="width: 100%;"
+						titleText="Engine Version"
+						selectedId="0"
+						items={[
+							{ id: '0', text: '0.0.1' },
+							{ id: '1', text: '0.0.2 Beta' },
+							{ id: '2', text: '0.0.3 Alpha' }
+						]}
+					/>
+				</ButtonSet>
+			</Column>
 		</Row>
 	</Grid>
 </Tile>
