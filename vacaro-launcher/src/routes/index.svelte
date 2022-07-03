777a43e16c7a321090d2278a891911e93547ef60
AZProductions <66299945+AZProductions@users.noreply.github.com>
2022-07-03 14:37:52 +0200

    chore: update scrollbar and index page

--- vacaro-launcher/src/routes/index.svelte
+++ vacaro-launcher/src/routes/index.svelte
@@ -1,52 +1,31 @@
-<!-- <script>
-	import {
-		ButtonSet,
-		Button,
-		Dropdown,
-		Tile,
-		ImageLoader,
-		InlineNotification
-	} from 'carbon-components-svelte';
-	var key = 0;
+<script>
+	import { Grid, Row, Column, Tile, AspectRatio } from 'carbon-components-svelte';
+	import FAQ from '../components/home/FAQ.svelte';
+	import YT from '../components/home/YT.svelte';
 </script>
 
 <Tile>
-	<Button
-		kind="ghost"
-		on:click={() => {
-			key++;
-		}}>Reload image</Button
-	>
-	<div class="center">
-		<InlineNotification
-			class="warning-card"
-			lowContrast
-			kind="warning"
-			title="Scheduled maintenance:"
-			subtitle="Maintenance will last 2-4 hours."
-		/>
-	</div>
-	{#key key}
-		<ImageLoader
-			fadeIn
-			ratio="16x9"
-			src="https://cdn.discordapp.com/attachments/281535090134351874/974038871811690516/4.jpg"
-		/>
-	{/key}
-	<ButtonSet>
-		<Dropdown
-			hideLabel
-			titleText="Engine Version"
-			selectedId="0"
-			items={[
-				{ id: '0', text: '0.0.1' },
-				{ id: '1', text: '0.0.2 Beta' },
-				{ id: '2', text: '0.0.3 Alpha' }
-			]}
-		/>
+	<Grid>
+		<Row>
+			<Column>Column</Column>
+			<Column>Column</Column>
+			<Column>Column</Column>
+			<Column>Column</Column>
+		</Row>
+	</Grid>
+</Tile>
 
-		<Button>Submit</Button>
-	</ButtonSet>
-</Tile> -->
-<h1>Welcome to Vacaro</h1>
-<p>Visit <a href="https://github.com/Vacaro">github.com/vacaro</a> to read the documentation</p>
+<Grid style="margin-top: 12px; width: 100%;">
+	<Row>
+		<Column aspectRatio="2x1">
+			<AspectRatio ratio="16x9">
+				<Tile style="height: 100% !important;">
+					<YT title="Music" url="https://www.youtube.com/embed/_8_X-M68c6Q" />
+				</Tile>
+			</AspectRatio>
+		</Column>
+		<Column aspectRatio="2x1">
+			<FAQ />
+		</Column>
+	</Row>
+</Grid>
